a26a706bf88f06181a0b57af006eaccd1209c304
msnt2 <100047634+msnt2@users.noreply.github.com>
2023-07-26 23:04:15 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x78b5205fce310f704c1828735f1aae7db4a6d0b5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
